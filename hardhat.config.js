1fc8c86e35a08444ed48c2522f23179889829d94
i-stam <i.stamelakos@gmail.com>
2021-03-20 16:17:38 +0000

    Add linting directives

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const path = require('path');
+
+require('@nomiclabs/hardhat-truffle5');
+
+const GAS_PRICE = 20e9; // 20 GWEI
+
+module.exports = {
+	GAS_PRICE,
+	solidity: {
+		compilers: [
+			{
+				version: '0.7.1',
+			},
+		],
+	},
+	paths: {
+		sources: './contracts',
+		tests: './test/',
+	},
+	defaultNetwork: 'hardhat',
+	networks: {
+		hardhat: {
+			gas: 12e6,
+			blockGasLimit: 12e6,
+			allowUnlimitedContractSize: true,
+			gasPrice: 10e9,
+		},
+		localhost: {
+			gas: 12e6,
+			blockGasLimit: 12e6,
+			url: 'http://localhost:8545',
+		},
+	},
+};
